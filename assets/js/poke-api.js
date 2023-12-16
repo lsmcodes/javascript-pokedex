5b8c1e529c6aa15fe07ce89f033c75ac8e30265c
Lorena Medeiros <lomedeiros@proton.me>
2023-12-16 19:18:29 -0300

    feat: add method to parse JSON into a Pokemon instance

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,5 +1,29 @@
 import { Pokemon } from "./models/pokemon-model";
 
 class PokeApi {
-    
+    convertPokemonDataToPokemon(json) {
+        const pokemon = new Pokemon();
+
+        const abilities = json.abilities.map((slot) => slot.ability.name);
+        const statsNames = json.stats.map((slot) => slot.stat.name);
+        const baseStats = json.stats.map((slot) => slot.base_stat);
+        const types = json.types.map((slot) => slot.type.name);
+        const [type] = types;
+        const typesUrls = json.types.map((slot) => slot.type.url);
+
+        pokemon.abilities = abilities;
+        pokemon.id = json.id;
+        pokemon.height = json.height;
+        pokemon.name = json.name;
+        pokemon.speciesUrl = json.species.url;
+        pokemon.sprite = json.sprites.other.home.front_default;
+        pokemon.statsNames = statsNames;
+        pokemon.baseStats = baseStats;
+        pokemon.types = types;
+        pokemon.type = type;
+        pokemon.typesUrls = typesUrls;
+        pokemon.weight = json.weight;
+
+        return pokemon;
+    }
 }

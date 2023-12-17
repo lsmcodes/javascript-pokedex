08d198cdfb0cdba3f202664560d0ab7196847953
Lorena Medeiros <lomedeiros@proton.me>
2023-12-17 14:59:20 -0300

    feat: add methods to convert Pokemon object to li and to start the conversion

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,5 +11,31 @@ class Main {
         this.pokemonList = document.querySelector(".pokemon-list");
         this.loadMoreButton = document.querySelector(".load-more-button");
     }
+
+    convertPokemonToLi(offset, limit) {
+        pokeApi.getAllPokemon(offset, limit).then((allPokemon) => allPokemon.forEach((pokemon) => {
+            const newPokemon = document.createElement('li');
+            newPokemon.classList.add('pokemon');
+
+            newPokemon.innerHTML = `
+            <div class="pokemon-info">
+                <span class="pokemon-number">${pokemon.id}</span>
+                <span class="pokemon-name">${pokemon.name}</span>
+                <div class="pokemon-types">
+                    ${pokemon.types.map((type) => `<span class="pokemon-type ${type}">${type}</span>`).join(' ')}
+                </div>
+            </div>
+            <div class="pokemon-image">
+                <img src="${pokemon.sprite}" alt="${pokemon.name}">
+            </div>`;
+
+            this.pokemonList.appendChild(newPokemon);
+        }));
+    }
+
+    init() {
+        this.convertPokemonToLi(pokedexConfig.offset, pokedexConfig.limit);
+    }
 }
 const main = new Main();
+main.init();
